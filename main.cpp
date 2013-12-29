3a52dd1cec1ec6f5b30f89a7440efc30a09e14ad
Jesper Stefansson <steffe.jst@gmail.com>
2013-12-29 02:35:33 +0100

    Basic SDL2 setup

--- main.cpp
+++ main.cpp
@@ -1,7 +1,52 @@
-#include <SDL/SDL.h>
+#include "SDL2/SDL.h"
 #include <iostream>
 
 int main(int argc, char* argv[]) {
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+
+	SDL_Init(SDL_INIT_VIDEO);
+	
+	window = SDL_CreateWindow(
+		"Pong",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		640,
+		480,
+		SDL_WINDOW_OPENGL
+	);
+
+	renderer = SDL_CreateRenderer(
+		window, -1, 
+		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC	
+	);
+
+
+	bool quit = false;
+	SDL_Event e;
+
+	while(!quit) {
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_QUIT)
+				quit = true;
+
+			if (e.type == SDL_KEYDOWN) 
+				quit = true;
+
+			if (e.type == SDL_MOUSEBUTTONDOWN)
+				quit = true;
+		}
+
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
+
+		SDL_Delay(2000);
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
 	std::cout << "Working";
 	return -1;
 }
